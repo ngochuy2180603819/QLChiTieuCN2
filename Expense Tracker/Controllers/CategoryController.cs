3c98243b90d172b2ee8beb36b730eab969364e94
huy <huy@gmail.com>
2024-06-04 18:32:31 +0700

    Add project files.

---
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Expense_Tracker.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CategoryController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+
+        }
+
+        // GET: Category
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userId = user.Id;
+            var categories = await _context.Categories
+                                          .Where(c => c.UserId == userId)
+                                          .ToListAsync();
+            return categories != null ?
+                View(categories) :
+                Problem("Entity set 'ApplicationDbContext.Categories' is null.");
+
+        }
+
+
+        // GET: Category/AddOrEdit
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Category());
+            else
+                
+                return View(_context.Categories.Find(id));
+        }
+
+        // POST: Category/AddOrEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("CategoryId,Title,Icon,Type")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.CategoryId == 0)
+                {
+                    var existingCategory = await _context.Categories
+                    .FirstOrDefaultAsync(c => c.Title == category.Title);
+                    if (existingCategory != null)
+                    {
+                        ModelState.AddModelError("", "Title already exists");
+                        return View(category);
+                    }
+                    _context.Add(category);
+                }
+                else
+                    _context.Update(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ Expense Tracker/Migrations/20240604110538_updateIdentity2.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    public partial class updateIdentity2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Categories",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_AspNetUsers_UserId",
+                table: "Categories",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_AspNetUsers_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Categories");
+        }
+    }
+}
