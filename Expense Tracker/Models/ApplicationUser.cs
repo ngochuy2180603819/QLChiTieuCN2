36c924b04625407848139e6022c65d86861d37ee
huy <huy@gmail.com>
2024-06-10 20:14:13 +0700

    image profile, change password

--- Expense Tracker/Models/ApplicationUser.cs
+++ Expense Tracker/Models/ApplicationUser.cs
@@ -7,5 +7,6 @@ namespace Expense_Tracker.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string ProfilePicture { get; set; }
     }
 }
