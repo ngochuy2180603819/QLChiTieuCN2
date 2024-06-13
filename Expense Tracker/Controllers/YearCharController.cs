7a70b6961a8db55e5cbf28d106ab2c33c9f5cc1d
huy <huy@gmail.com>
2024-06-13 18:14:35 +0700

    3

--- Expense Tracker/Controllers/YearCharController.cs
+++ Expense Tracker/Controllers/YearCharController.cs
@@ -1,4 +1,4 @@
-﻿using Expense_Tracker.Models;
+﻿﻿using Expense_Tracker.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -22,15 +22,18 @@ namespace Expense_Tracker.Controllers
         public async Task<IActionResult> IndexAsync(DateTime? Selected, String type)
         {
             var userId = _userManager.GetUserId(User);
-            DateTime time= new DateTime(DateTime.Today.Year);
+            DateTime StartDate;
+            DateTime EndDate;
 
             if (Selected.HasValue)
             {
-                time = new DateTime(Selected.Value.Year);
+                StartDate = new DateTime(Selected.Value.Year, 1, 1);
+                EndDate = StartDate.AddYears(1).AddDays(-1);
                 List<Transaction> selectedTransactions = await _context.Transactions
-                        .Include(x => x.Category)
-                        .Where(y => y.UserId == userId && y.Date.Year == Selected.Value.Year)
-                        .ToListAsync();
+                .Include(x => x.Category)
+                .Where(y => y.UserId == userId && y.Date >= StartDate && y.Date <= EndDate)
+                .ToListAsync();
+
                 if (type == "Expense" || type == "Income")
                 {
                     // Prepare data for the doughnut chart
@@ -46,16 +49,15 @@ namespace Expense_Tracker.Controllers
                         .OrderByDescending(l => l.amount)
                         .ToList();
                 }
-                else
-                {
-                    // Invalid type provided
+            }
+            else
+            {
+
                     ViewBag.DoughnutChartData = null;
-                }
 
             }
 
-
-            ViewBag.Selected = Selected;
+            ViewBag.SelectedDate = Selected;
             ViewBag.Type = type;
 
             return View();
